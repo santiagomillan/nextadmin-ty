f85d15faeb1bd698b9663193f0a0a0851af08518
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-22 00:46:23 -0500

    Dashboard Design 48:20

---
+++ app/ui/dashboard/chart/chart.jsx
@@ -0,0 +1,11 @@
+import styles from "./chart.module.css";
+
+const Chart = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Chart</h1>
+        </div>
+    )
+}
+
+export default Chart
---
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -0,0 +1,11 @@
+import styles from "./rightbar.module.css";
+
+const Rightbar = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Rightbar</h1>
+        </div>
+    )
+}
+
+export default Rightbar
