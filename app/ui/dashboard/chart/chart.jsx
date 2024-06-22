1afedb1ea9369fdeb5231aaaf3f34db97577f9eb
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-22 10:32:36 -0500

    Feature: Rightbar Design 59:00

--- app/ui/dashboard/chart/chart.jsx
+++ app/ui/dashboard/chart/chart.jsx
@@ -1,6 +1,7 @@
 "use client"
 import styles from "./chart.module.css";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+// CartesianGrid,
 
 const data = [
     {
