a57f766d20daec9db2d4d685d48ac74573a260dc
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-23 15:23:03 -0500

    Feature: Products  1:18:40

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,6 +1,54 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination"
+import Search from "@/app/ui/dashboard/search/search"
+import Image from "next/image"
+import Link from "next/link"
+import styles from "@/app/ui/dashboard/products/products.module.css"
+
 const ProductsPage = () => {
     return(
-        <div>ProductsPage</div>
+        <div className={styles.container}>
+        <div className={styles.top}>
+            <Search placeholder="Search for a product..."/>
+            <Link href="/dashboard/products/add">
+                <button className={styles.addButton}>Add new</button>
+            </Link>
+        </div>
+        <table className={styles.table}>
+            <thead>
+                <tr>
+                    <td>Title</td>
+                    <td>Description</td>
+                    <td>Price</td>
+                    <td>Created At</td>
+                    <td>Stock</td>
+                    <td>Actions</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <div className={styles.product}>
+                            <Image src="/noproduct.jpg" alt="" width={40} height={40} className={styles.productImage}/>
+                            Iphone
+                        </div>
+                    </td>
+                    <td>Desc</td>
+                    <td>$999</td>
+                    <td>13.01.2022</td>
+                    <td>22</td>
+                    <td>
+                        <div className={styles.buttons}>
+                            <Link href="/">
+                                <button className={`${styles.button} ${styles.view}`}>View</button>
+                            </Link>
+                            <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <Pagination/>
+    </div>
     )
 
 }
