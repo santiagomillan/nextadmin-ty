76cfa4c5d778cbc15f7da442de45c1ea455d551f
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-25 00:24:32 -0500

    Feature: Single User and Product Page Design 1:39:25

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -11,27 +11,29 @@ const SingleUsersPage = () => {
                 John Doe
             </div>
             <div className={styles.formContainer}>
-                <label>Username</label>
-                <input type="text" name="username" placeholder="johndoe"/>
-                <label>Email</label>
-                <input type="email" name="email" placeholder="johndoe@email.com"/>
-                <label>Password</label>
-                <input type="password" name="password" placeholder="password"/>
-                <label>Phone</label>
-                <input type="text" name="phone" placeholder="+123456789"/>
-                <label>Addres</label>
-                <input type="text" name="address" placeholder="calle"/>
-                <label>Is Admin?</label>
-                <select name="isAdmin" id="isAdmin">
-                    <option value={true}>Yes</option>
-                    <option value={false}>No</option>
-                </select>
-                <label>Is Active?</label>
-                <select name="isActive" id="isActive">
-                    <option value={true}>Yes</option>
-                    <option value={false}>No</option>
-                </select>
-
+                <form action="" className={styles.form}>
+                    <label>Username</label>
+                    <input type="text" name="username" placeholder="johndoe"/>
+                    <label>Email</label>
+                    <input type="email" name="email" placeholder="johndoe@email.com"/>
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="password"/>
+                    <label>Phone</label>
+                    <input type="text" name="phone" placeholder="+123456789"/>
+                    <label>Addres</label>
+                    <input type="text" name="address" placeholder="calle"/>
+                    <label>Is Admin?</label>
+                    <select name="isAdmin" id="isAdmin">
+                        <option value={true}>Yes</option>
+                        <option value={false}>No</option>
+                    </select>
+                    <label>Is Active?</label>
+                    <select name="isActive" id="isActive">
+                        <option value={true}>Yes</option>
+                        <option value={false}>No</option>
+                    </select>
+                    <button>Update</button>
+                </form>
             </div>
         </div>
     )
