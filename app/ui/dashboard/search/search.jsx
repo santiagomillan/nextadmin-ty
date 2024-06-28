078e7eb61e69726f59e2c28ee95ec132b8461cc8
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-27 23:48:02 -0500

    Feature: Captura de param y mostrarlo en la url

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,11 +1,31 @@
+"use client"
 import { MdSearch } from 'react-icons/md'
 import styles from './search.module.css'
+import { usePathname, useSearchParams, useRouter } from 'next/navigation'
 
 const Search = ({placeholder}) => {
+  const searchParams = useSearchParams();
+  const {replace} = useRouter()
+  const pathname = usePathname()
+
+  const handleSearch = (e)=>{
+     const params = new URLSearchParams(searchParams);
+     params.set('q', e.target.value)
+     replace(`${pathname}?${params}`)
+  }
+
+
+ 
+
+  // params.set("test", "value")
+
+  
+  // console.log(pathname)
+  // console.log(params)
     return (
       <div className={styles.container}>
         <MdSearch/>
-        <input type="text" placeholder={placeholder} className={styles.input}/>
+        <input type="text" placeholder={placeholder} className={styles.input} onChange={handleSearch}/>
       </div>
     )
 }
