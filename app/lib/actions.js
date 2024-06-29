b810426501a1a0db4deb1d645d02b5be06f461dc
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-29 01:32:46 -0500

    Feature: action addUser p1 2:36:46

---
+++ app/lib/actions.js
@@ -0,0 +1,17 @@
+import { User } from "./models"
+import { connectToDB } from "./utils"
+
+export const addUser = async(formData)=>{
+    "use server"
+    const {username, email,password,phone, address, isAdmin, isActive} = Object.fromEntries(formData)
+
+    try {
+        connectToDB()
+        const newUser = new User({username, email,password,phone, address, isAdmin, isActive})
+        await newUser.save()
+    } catch (error) {
+        console.error(error)
+        throw new Error("Failed to create user!")
+    }
+
+}
