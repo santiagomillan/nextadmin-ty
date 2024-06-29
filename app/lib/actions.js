f4de9b515925f0317bb7418f103c33ab443eb82b
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-29 11:07:13 -0500

    Feature: addProduct 2:44:52

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,17 +1,43 @@
-import { User } from "./models"
+"use server"
+import { revalidatePath } from "next/cache"
+import { Products, User } from "./models"
 import { connectToDB } from "./utils"
+import { redirect } from "next/navigation"
+import bcrypt from "bcrypt"
+
 
 export const addUser = async(formData)=>{
-    "use server"
+    // "use server"
     const {username, email,password,phone, address, isAdmin, isActive} = Object.fromEntries(formData)
 
     try {
         connectToDB()
-        const newUser = new User({username, email,password,phone, address, isAdmin, isActive})
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+        const newUser = new User({username, email,password:hashedPassword,phone, address, isAdmin, isActive})
         await newUser.save()
     } catch (error) {
         console.error(error)
         throw new Error("Failed to create user!")
     }
+    revalidatePath("/dashboard/users")
+    redirect("/dashboard/users")
+}
 
+export const addProduct = async(formData)=>{
+    // "use server"
+    const {title, desc, price, stock, color, size  } = Object.fromEntries(formData)
+
+    try {
+        connectToDB()
+        // const salt = await bcrypt.genSalt(10)
+        // const hashedPassword = await bcrypt.hash(password, salt)
+        const newProduct = new Products({title, desc, price, stock, color, size})
+        await newProduct.save()
+    } catch (error) {
+        console.error(error)
+        throw new Error("Failed to create product!")
+    }
+    revalidatePath("/dashboard/products")
+    redirect("/dashboard/products")
 }
