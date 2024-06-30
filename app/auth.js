bb303ab932eb6ae11c5a07a092337a5263d62b1d
Santiago Millan <santiagomillanpardo@gmail.com>
2024-06-30 15:06:25 -0500

    Feature: Next-Auth Protected Route with Middleware 3:07:01

---
+++ app/auth.js
@@ -0,0 +1,82 @@
+// import NextAuth from "next-auth";
+// import CredentialsProviders from "next-auth/providers/credentials";
+// import { authConfig } from "./authconfig";
+// import { connectToDB } from "./lib/utils";
+// import { User } from "./lib/models";
+// import bcrypt from "bcrypt";
+
+// const login = async (credentials)=>{
+//     try {
+//         connectToDB()
+//         const user = await User.findOne({username: credentials.username})
+//         if(!user) throw new Error("Wrong credentials!")
+
+//         const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+//         if(!isPasswordCorrect) throw new Error("Wrong credentials!")
+
+//         return user
+//     } catch (error) {
+//         console.log(error)
+//         throw new Error("Failed to login!")
+//     }
+// }
+
+// export const {signIn, signOut, auth} = NextAuth({
+//     ...authConfig,
+//     providers:[
+//         CredentialsProviders({
+//             async authorize(credentials) {
+//                 try {
+//                     const user = await login(credentials)
+//                     return user
+//                 } catch (error) {
+//                     return null
+//                 }
+//             }
+//         })
+//     ]
+// })
+
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { authConfig } from "./authconfig";
+import { connectToDB } from "./lib/utils";
+import { User } from "./lib/models";
+import bcrypt from "bcrypt";
+
+const login = async (credentials) => {
+  try {
+    connectToDB();
+    const user = await User.findOne({ username: credentials.username });
+
+    if (!user || !user.isAdmin) throw new Error("Wrong credentials!");
+
+    const isPasswordCorrect = await bcrypt.compare(
+      credentials.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) throw new Error("Wrong credentials!");
+
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to login!");
+  }
+};
+
+export const { signIn, signOut, auth } = NextAuth({
+  ...authConfig,
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        try {
+          const user = await login(credentials);
+          return user;
+        } catch (err) {
+          return null;
+        }
+      },
+    }),
+  ]
+});
--- app/authconfig.js
+++ app/authconfig.js
@@ -1,18 +1,40 @@
+
+// export const authConfig = {
+//     providers:[],
+//     pages: {
+//         signIn: "/login"
+//     },
+//     callbacks:{
+//         authorized({auth,request}){
+//             const isLoggedIn = auth?.user;
+//             const isOnDashboard = request.nextURL.pathname.startsWith("/dashboard");
+//             if(isOnDashboard){
+//                 if (isLoggedIn) return true
+//                 return false
+//             }else if(isLoggedIn){
+//                 return Response.redirect(new URL("/dashboard", request.nextURL))
+//             }
+//             return true
+//         }
+//     }
+// }
+
 export const authConfig = {
+    providers:[],
     pages: {
-        signIn: "/login"
+      signIn: "/login",
     },
-    callbacks:{
-        authorized({auth,request}){
-            const isLoggedIn = auth?.user
-            const isOnDashboard = request.nextURL.pathname.startsWith("/dashboard")
-            if(isOnDashboard){
-                if (isLoggedIn) return true
-                return false
-            }else if(isLoggedIn){
-                return Response.redirect(new URL("/dashboard", request.nextURL))
-            }
-            return true
+    callbacks: {
+      authorized({ auth, request }) {
+        const isLoggedIn = auth?.user;
+        const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
+        if (isOnDashboard) {
+          if (isLoggedIn) return true;
+          return false;
+        } else if (isLoggedIn) {
+          return Response.redirect(new URL("/dashboard", request.nextUrl));
         }
-    }
-}
+        return true;
+      },
+    },
+  };
